a60cbd570fa554e8429088d5e291e59f67e85c7c
Michael Kerekes <mikeke@mikeke.com>
2020-08-06 10:41:06 -0700

    Update prefix.hs

--- prefix.hs
+++ prefix.hs
@@ -1,35 +1,33 @@
 ------------------------------------------------------------------------------
---  types
+--  AST
 ------------------------------------------------------------------------------
 
+data E             = EX X | EApply E Operator E
 type X             = String
 type Operator      = X
-data E             = EX X | EApply E Operator E
-data Associativity = LeftAssociative | RightAssociative
-data Precidence    = Precidence Associativity Int
 
 ------------------------------------------------------------------------------
---  Precidence
+--  Precedence
 ------------------------------------------------------------------------------
 
+data Precedence    = Precedence Associativity Int
 data Associativity = LeftAssociative | RightAssociative
-data Precidence    = Precidence Associativity Int
 
-instance Zero Precidence where zero = Precidence LeftAssociative 0
+zero = Precedence LeftAssociative 0
 
-higher :: Precidence -> Precidence -> Bool
-higher (Precidence _ x) (Precidence LeftAssociative  y) = y >  x
-higher (Precidence _ x) (Precidence RightAssociative y) = y >= x
+higher :: Precedence -> Precedence -> Bool
+higher (Precedence _ x) (Precedence LeftAssociative  y) = y >  x
+higher (Precedence _ x) (Precedence RightAssociative y) = y >= x
 
 ------------------------------------------------------------------------------
 --  algorithm
 ------------------------------------------------------------------------------
 
-delimit :: (Operator -> Precidence) -> [X] -> E
+delimit :: (Operator -> Precedence) -> [X] -> E
 delimit pop []       = EX "<error>"
 delimit pop (x : xs) = first (go zero (EX x) xs)
   where
-    go :: Precidence -> E -> [X] -> (E, [X])
+    go :: Precedence -> E -> [X] -> (E, [X])
     go pLeft eLeft (op : x : xs) | higher pLeft (pop op) = let (eRight, xs') = go (pop op) (EX x) xs in go pLeft (EApply eLeft op eRight) xs'
     go pLeft eLeft xs                                    = (eLeft, xs)
 
@@ -37,11 +35,11 @@ delimit pop (x : xs) = first (go zero (EX x) xs)
 --  usage
 ------------------------------------------------------------------------------
 
-ops :: Map X Precidence
-ops = toT ( [ ("+", Precidence LeftAssociative  1)
-            , ("*", Precidence LeftAssociative  2)
-            , ("^", Precidence RightAssociative 3)
-            ] :: List (X, Precidence)
+ops :: Map X Precedence
+ops = toT ( [ ("+", Precedence LeftAssociative  1)
+            , ("*", Precedence LeftAssociative  2)
+            , ("^", Precedence RightAssociative 3)
+            ] :: List (X, Precedence)
           )
 
 e = delimit (lookupDefault zero ops) [ "x", "+", "y", "^", "z", "^", "m", "*", "w", "+", "n" ]
